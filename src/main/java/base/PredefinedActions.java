bb1d129d062b1e4e3bda0421ffad72336a0b221c
Deepak <deepak.patil793@gmail.com>
2023-08-22 00:35:27 +0530

    minorchanges

--- src/main/java/base/PredefinedActions.java
+++ src/main/java/base/PredefinedActions.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import org.apache.commons.io.FileUtils;
-import org.apache.poi.ss.formula.atp.Switch;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
